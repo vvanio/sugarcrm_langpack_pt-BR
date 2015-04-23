40b9dc121c95a99c7ce4ca7a2f00a9ecdd909bbf
Vanio Vieira <vvanio@yahoo.com.br>
2015-04-23 13:46:53 -0300

    6.5.x

--- include/javascript/yui3/build/datatype/lang/datatype-date-format_pt-BR.js
+++ include/javascript/yui3/build/datatype/lang/datatype-date-format_pt-BR.js
@@ -5,4 +5,11 @@
  version: 3.3.0
  build: 3167
  */
+/*
+ *********************************************************************************
+ * Description:  Defines the Brazilian Portuguese language pack for the base application.
+ * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc.
+ * All Rights Reserved.
+ * Contributor(s): Vanio Vieira (vvanio@yahoo.com.br).
+ ********************************************************************************/
 YUI.add("lang/datatype-date-format_pt-BR",function(a){a.Intl.add("datatype-date-format","pt-BR",{"a":["Dom","Seg","Ter","Qua","Qui","Sex","Sáb"],"A":["Domingo","Segunda","Terça","Quarta","Quinta","Sexta","Sábado"],"b":["Jan","Fev","Mar","Abr","Mai","Jun","Jul","Ago","Set","Out","Nov","Dez"],"B":["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"],"c":"%a, %d de %b de %Y %Hh%Mmin%Ss %Z","p":["AM","PM"],"P":["am","pm"],"x":"%d/%m/%y","X":"%Hh%Mmin%Ss"});},"3.3.0");
